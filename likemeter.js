1342ee67924b9787b6fc4c7eeb76f01ecfbe92d5
Andrey Kostakov <bps@dzen.ws>
2014-01-02 20:47:30 +0400

    modify result format

--- likemeter.js
+++ likemeter.js
@@ -8,14 +8,34 @@
 			};
 			var options = $.extend({}, defaults, options);
 			var results = {};
+			var social_ok = [];
 
     		var social = {
-    			facebook: facebook,
+    			'facebook': facebook,
+    			'twitter': twitter,
     		}
 
-    		function collect_result(result){
-    			var results = $.extend({}, results, result);
-    			Object.keys(results).length >= options.networks.length && options.callback(results);
+    		$.each(options.urls, function(i, url){
+    			results[url] = {};
+    			$.each(options.networks, function(i, network){
+    				results[url][network] = {}
+    			});
+    		});
+
+    		function collect_result(result, network){
+    			social_ok.push(network);
+    			$.each(result, function(i, res){
+    				if(res.likes){
+    					results[res.url][network]['likes'] = res.likes;
+    				}
+    				if(res.shares){
+    					results[res.url][network]['shares'] = res.shares;
+    				}
+    			});
+
+    			if(social_ok.length >= options.networks.length){    				
+    				options.callback(results);
+    			}
     		}
 
     		$.each(options.networks, function(i, network){
@@ -27,22 +47,45 @@
     			$.ajax({
 			        type: "GET",
 			        dataType: "jsonp",
-			        url: 'http://api.facebook.com/restserver.php',
+			        url: 'https://api.facebook.com/restserver.php',
 			        data: {'method': 'links.getStats', 'urls': urls, 'format': 'json'}
 			    })
 			    .done(function (obj){
-		        	var results = {};
+	    			var results = [];
 		        	$.each(obj, function(i, res){
-		        		results[res.url] = {likes: res.like_count, shares: res.share_count};
+		        		results.push({url: res.url, likes: res.like_count, shares: res.share_count})
 		        	});
-		            callback({'facebook': results});
+		            callback(results, 'facebook');
 			    })
 			    .fail(function(){
-		        	callback({'facebook': {}});
+			    	callback([], 'facebook');
 			    })
+    		}
+
+    		function twitter(urls, callback){
+    			var results = [];
+    			$.each(urls, function(i, url){
+					$.ajax({
+				        type : "GET",
+	        			dataType : "jsonp",
+	        			url : 'https://cdn.api.twitter.com/1/urls/count.json',
+	        			data : {'url': url},
+				    })
+				    .always(function(data, status){
+				    	if(status == 'success'){
+				    		results.push({url: url, shares: data.count})
+				    	}
+				    	else{
+				    		results.push({url: url})
+				    	}
+				    	if(Object.keys(results).length >= urls.length){
+				    		callback(results, 'twitter');
+				    	}
+				    })
+    			});
     		}
         }
     });
 })(jQuery);
 
-$.LikeMeter({urls: ['http://mail.ru', 'http://yandex.ru'], networks: ['facebook']});
+$.LikeMeter({urls: ['http://mail.ru', 'http://yandex.ru'], networks: ['facebook', 'twitter']});
